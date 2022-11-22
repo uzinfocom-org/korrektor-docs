95a6ab29b1c089348f9539907a5b1b191f09b0a5
Manuchehr Usmonov <con9799@mail.ru>
2022-11-22 16:38:08 +0500

    Changed language

--- components/news-index.js
+++ components/news-index.js
@@ -1,10 +1,27 @@
+import { useRouter } from "next/router";
 import { getPagesUnderRoute } from "nextra/context";
 import Link from "next/link";
 
-export default function BlogIndex({ more = "Read more" }) {
-  return getPagesUnderRoute("/blog").map((page) => {
+const NEWS_TRANSLATIONS = {
+  "en-US": {
+    read_more: "Read more",
+  },
+  uz: {
+    read_more: "Batafsil",
+  },
+  ru: {
+    read_more: "Читать далее",
+  }
+};
+
+export default function NewsIndex() {
+  const { locale, defaultLocale } = useRouter();
+  return getPagesUnderRoute("/news").map((page) => {
     // Alias `<a>` to avoid it being replaced by MDX components.
     const A = "a";
+    const newsText = (key) =>
+      NEWS_TRANSLATIONS[locale]?.[key] ??
+      NEWS_TRANSLATIONS[defaultLocale][key];
     return (
       <div className="" key={page.name.toString()}>
         <h3>
@@ -16,7 +33,7 @@ export default function BlogIndex({ more = "Read more" }) {
         </h3>
         <p className="opacity-80">
           {page.frontMatter?.description}{" "}
-          <Link href={page.route}>{more + " →"}</Link>
+          <Link href={page.route}>{newsText("read_more") + " →"}</Link>
         </p>
         {page.frontMatter?.date ? (
           <p className="opacity-50 text-sm">{page.frontMatter.date}</p>
