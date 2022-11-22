22161973cca52277ab7cab4d563fee8df19a0f84
Manuchehr Usmonov <con9799@mail.ru>
2022-11-22 15:43:40 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,55 +8,6 @@ const withNextra = require("nextra")({
 module.exports = withNextra({
   i18n: {
     locales: ["en-US", "ru", "uz"],
-    defaultLocale: "en-US",
-  },
-  redirects: () => {
-    return [
-      {
-        source: "/docs",
-        destination: "/docs/getting-started",
-        statusCode: 301,
-      },
-      {
-        source: "/advanced/performance",
-        destination: "/docs/advanced/performance",
-        statusCode: 301,
-      },
-      {
-        source: "/advanced/cache",
-        destination: "/docs/advanced/cache",
-        statusCode: 301,
-      },
-      {
-        source: "/docs/cache",
-        destination: "/docs/advanced/cache",
-        statusCode: 301,
-      },
-      {
-        source: "/change-log",
-        destination: "/docs/change-log",
-        statusCode: 301,
-      },
-      {
-        source: "/blog/swr-1",
-        destination: "/blog/swr-v1",
-        statusCode: 301,
-      },
-      {
-        source: "/docs",
-        destination: "/docs/getting-started",
-        statusCode: 302,
-      },
-      {
-        source: "/useful",
-        destination: "/useful/aur",
-        statusCode: 302,
-      },
-      {
-        source: "/download/latest",
-        destination: "/api/download/latest",
-        statusCode: 302,
-      },
-    ];
+    defaultLocale: "uz",
   },
 });
